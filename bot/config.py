8a3f0214eb34f45a6fa44c8f64e0d95e527c6a51
Mihai Alexandru Pacurar <mihai7alex@gmail.com>
2023-04-17 23:59:11 +0100

    Update config to get env vars fix

--- bot/config.py
+++ bot/config.py
@@ -5,13 +5,13 @@ from pathlib import Path
 config_dir = Path(__file__).parent.parent.resolve() / "config"
 
 # config parameters
-telegram_token = os.getenv["telegram_token"]
-openai_api_key = os.getenv["openai_api_key"]
+telegram_token = os.getenv("telegram_token")
+openai_api_key = os.getenv("openai_api_key")
 use_chatgpt_api = os.getenv("use_chatgpt_api", True)
-allowed_telegram_usernames = os.getenv["allowed_telegram_usernames"]
-new_dialog_timeout = os.getenv["new_dialog_timeout"]
+allowed_telegram_usernames = os.getenv("allowed_telegram_usernames")
+new_dialog_timeout = os.getenv("new_dialog_timeout")
 enable_message_streaming = os.getenv("enable_message_streaming", True)
-mongodb_uri = f"mongodb://mongo:{os.getenv['MONGODB_PORT']}"
+mongodb_uri = f"mongodb://mongo:{os.getenv('MONGODB_PORT')}"
 
 # chat_modes
 with open(config_dir / "chat_modes.yml", 'r') as f:
